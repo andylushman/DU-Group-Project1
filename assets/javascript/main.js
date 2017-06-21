8649c52f85d6aadeed1aaf01b8cd5d62abb0621c
Andy Lushman <andylushman@gmail.com>
2017-06-21 10:06:44 -0600

    Created initmap function and added a marker

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -10,12 +10,18 @@
 //=======================
 
 function initMap() {
+  //Map Options
   var option = {
     zoom: 8,
     center: {lat: 39.7392, lng: -104.9903}
   }
-
+  //Putting the new map on the page
   var map = new google.Map($("#MAPDIVID"), options)
+  //Add marker
+  var marker = new google.maps.Marker({
+    position: {lat: 39.758259, lng: -105.007198},
+    map: map
+  })
 };
 
 
