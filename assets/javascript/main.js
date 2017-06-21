15eb919520a9a2b95d9306e8469313e8745b718b
Andy Lushman <andylushman@gmail.com>
2017-06-21 09:57:23 -0600

    Added css for the map

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -9,6 +9,14 @@
 //FUNCTIONS
 //=======================
 
+function initMap() {
+  var option = {
+    zoom: 8,
+    center: {lat: 39.7392, lng: -104.9903}
+  }
+
+  var map = new google.Map($("#MAPDIVID"), options)
+};
 
 
 //=======================
