8fb8cf194548540f66f6d55852210cf72bbdeba1
Andy Lushman <andylushman@gmail.com>
2017-06-21 10:42:30 -0600

    Changed my classes to match the ones that Don created in index.html

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -16,7 +16,7 @@ function initMap() {
     center: {lat: 39.7392, lng: -104.9903}
   }
   //Putting the new map on the page
-  var map = new google.Map($("#MAPDIVID"), options)
+  var map = new google.Map($(".map"), options)
   //Add marker
   var marker = new google.maps.Marker({
     position: {lat: 39.758259, lng: -105.007198},
