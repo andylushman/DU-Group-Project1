1073a843aae743823a61121247e72060e773cc96
Andy Lushman <andylushman@gmail.com>
2017-06-23 13:42:10 -0600

    Added geolocation function

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,7 +2,7 @@
 //GLOBAL VARIABLES
 //=======================
 var map;
-var infowindow;
+var infoWindow;
 
 
 
@@ -14,14 +14,45 @@ var infowindow;
 //Initalize Function
 function initMap() {
   //Denver coordinates
+  var currentLocation = () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Location found.');
+        infoWindow.open(map);
+        map.setCenter(pos);
+      }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  }//end currentLocation()
+
+  //If no geolocation service, run this function
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                          'Error: The Geolocation service failed.' :
+                          'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+  }; //end handleLocationError()
+
   var denver = {lat:39.7392,lng:-104.9903};
   //Map that is loaded on page
   map = new google.maps.Map(document.getElementById("map"), {
     center: denver,
-    zoom: 15
+    zoom: 14
   });
   //
-  infowindow = new google.maps.InfoWindow();
+  infoWindow = new google.maps.InfoWindow();
   var service = new google.maps.places.PlacesService(map);
   //Search based on bar
   service.nearbySearch({
@@ -35,7 +66,7 @@ function initMap() {
 function callback(results, status) {
   if (status === google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
-      createMarker(results[i]); //Puts each location in the createMarker function
+      createMarker(results[i]); //Puts each location in loop through the createMarker function
     };
   };
 }; //End callback()
@@ -57,8 +88,8 @@ function createMarker(place) {
   });
   //When a marker is clicked, run this function
   google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent("<h4>" + place.name + "</h4><h5> Place ID:" + place.place_id + "</h5>");
-    infowindow.open(map, this);
+    infoWindow.setContent("<h4>" + place.name + "</h4><h5> Place ID:" + place.place_id + "</h5>");
+    infoWindow.open(map, this);
   });
 }; //end createMarker()
 
