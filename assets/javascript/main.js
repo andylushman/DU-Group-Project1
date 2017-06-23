25581a4ba0aeb2dcc41681357ed6cf06ba461563
Andy Lushman <andylushman@gmail.com>
2017-06-23 13:05:02 -0600

    Added comments to main.js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -11,45 +11,48 @@ var infowindow;
 //FUNCTIONS
 //=======================
 
-
-
+//Initalize Function
 function initMap() {
+  //Denver coordinates
   var denver = {lat:39.7392,lng:-104.9903};
-
+  //Map that is loaded on page
   map = new google.maps.Map(document.getElementById("map"), {
     center: denver,
     zoom: 15
   });
-
+  //
   infowindow = new google.maps.InfoWindow();
   var service = new google.maps.places.PlacesService(map);
+  //Search based on bar
   service.nearbySearch({
     location: denver,
     radius: 10000,
     type: ["bar"]
-  }, callback);
-}
+  }, callback); //Calls callback function
+}; // End initMap()
 
+//Callback function
 function callback(results, status) {
   if (status === google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
-      createMarker(results[i]);
-    }
-  }
-}
+      createMarker(results[i]); //Puts each location in the createMarker function
+    };
+  };
+}; //End callback()
 
+//Create Marker Function
 function createMarker(place) {
   var placeLoc = place.geometry.location;
   var marker = new google.maps.Marker({
     map: map,
     position: place.geometry.location
   });
-
+  //When a marker is clicked, run this function
   google.maps.event.addListener(marker, 'click', function() {
     infowindow.setContent("<h4>" + place.name + "</h4><h5> Place ID:" + place.place_id + "</h5>");
     infowindow.open(map, this);
   });
-}
+}; //end createMarker()
 
 
 
