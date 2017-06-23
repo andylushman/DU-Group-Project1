217cced6dfab92d5d23201c8ba0e1199bfa03eea
Andy Lushman <andylushman@gmail.com>
2017-06-23 12:59:34 -0600

    Created new function to populate map with place name and place id

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,23 +1,9 @@
 //=======================
 //GLOBAL VARIABLES
 //=======================
+var map;
+var infowindow;
 
-// Array of markers
-var markers = [
-  {
-    coords:{lat:39.758241,lng:-105.007269},
-    iconImage:"https:developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-    content:"<h1>Denver Beer Co</h1>"
-  },
-  {
-    coords:{lat:39.758641,lng:-105.009066},
-    iconImage:"https:developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-    content:"<h1>Ale House</h1>"
-  },
-  // {
-  //   coords:{lat:,lng:-}
-  // }
-];
 
 
 
@@ -25,52 +11,45 @@ var markers = [
 //FUNCTIONS
 //=======================
 
-function initMap(){
-  // Map options
-  var options = {
-    zoom: 13,
-    center: {lat:39.7392,lng:-104.9903}
-  }
 
-  // New map
-  var map = new google.maps.Map(document.getElementById("map"), options);
 
-  // Loop through markers
-  for(var i = 0;i < markers.length;i++){
-    // Add marker
-    addMarker(markers[i]);
-  }
+function initMap() {
+  var denver = {lat:39.7392,lng:-104.9903};
 
-  // Add Marker Function
-  function addMarker(props){
-    var marker = new google.maps.Marker({
-      position:props.coords,
-      map:map,
-      //icon:props.iconImage
-    });
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: denver,
+    zoom: 15
+  });
 
-    // Check for customicon
-    if(props.iconImage){
-      // Set icon image
-      // var icon = new google.maps.Icon({
-      //   url: ,
-      //   scaledSize: new google.maps.Size(15, 15)
-      // });
-      marker.setIcon(props.iconImage);
-    }
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: denver,
+    radius: 10000,
+    type: ["bar"]
+  }, callback);
+}
 
-    // Check content
-    if(props.content){
-      var infoWindow = new google.maps.InfoWindow({
-        content:props.content
-      });
-
-      marker.addListener("click", function(){
-        infoWindow.open(map, marker);
-      });
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
     }
   }
-} //initMap();
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent("<h4>" + place.name + "</h4><h5> Place ID:" + place.place_id + "</h5>");
+    infowindow.open(map, this);
+  });
+}
 
 
 
@@ -78,14 +57,3 @@ function initMap(){
 //=======================
 //MAIN PROCESS
 //=======================
-// var googleMapsAPIKey = "AIzaSyA6mcqCJFzPLd_-UydR4nQRyb5O8kvIShM";
-// var googleMapsAPIQuery =  "https://maps.googleapis.com/maps/api/js/json?key=AIzaSyA6mcqCJFzPLd_-UydR4nQRyb5O8kvIShM&libraries=places";
-// $.ajax ({
-//   url: googleMapsAPIQuery,
-//   headers: {
-//     "Access-Control-Allow-Origin": true
-//   },
-//   method: 'get'
-// }).done(function (response){
-//   console.log(response);
-// });
